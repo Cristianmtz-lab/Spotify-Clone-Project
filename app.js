3266106622fc86429284e4d66e33c6129ae3737c
blackcode2 <crija19@gmail.com>
2025-03-06 21:37:56 -0500

    routes public login, style reset, variable, typography, login

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 "use strict";
 
 // node module
-
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-// initial express app 
+// custom modules
+const login = require('./src/routes/login.route');
 
+// initial express app 
 const express = require('express');
 const app = express();
+
+// enable cors & cookie parser
+app.disable('x-powered-by');
+app.use(cors()).use(cookieParser());
+app.use(express.json());
 
 // ejs
 app.set('view engine', 'ejs');
@@ -16,13 +22,8 @@ app.set('view engine', 'ejs');
 // setting static directory
 app.use(express.static(`${__dirname}/public`));
 
-// enable cors & cookie parser
-app.use(cors()).use(cookieParser());
-
 // login page 
-app.get('/login', (req, res) => {
-  res.send('login');
-});
+app.use('/login', login);
 
 app.listen(5000, () => {
   console.log(`Server listening att http://localhost:5000`);
